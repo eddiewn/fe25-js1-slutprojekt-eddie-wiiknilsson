6478f4d7192d302d9ffb6893735187128a27275a
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-12-23 16:23:52 +0100

    Added anime.js animation

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -3,3 +3,12 @@ const browseButton = document.getElementById('browse-button');
 browseButton.addEventListener('click', () => {
     window.location.href = 'browser.html';
 });
+
+    anime({
+        targets: '.box',  
+        translateX: 250, 
+        duration: 1000, 
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        loop: true,
+    });    
